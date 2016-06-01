420ec7ad4b0e6c72df69e22bc4bf566b39ef83e6
Kevin Bullaughey <kbullaughey@gmail.com>
2016-06-01 14:09:51 -0400

    now moving player before moving boat. This allowed it to train much easier. getting about 2/3 wins

--- toy.lua
+++ toy.lua
@@ -30,22 +30,24 @@ cmd:text()
 cmd:text('Toy game for reinfocement learning')
 cmd:text()
 cmd:text('Options')
-cmd:option('-mode', 'play|train', 'start a server for playing in a web browser or training')
+cmd:option('-mode', '', 'start a server for playing in a web browser or training (play|train|test)')
 cmd:option('-batch', 8, 'batch size')
 cmd:option('-size', 10, 'length of world')
-cmd:option('-hidden', 20, 'GRU hidden size')
+cmd:option('-hidden', 20, 'hidden size')
 cmd:option('-capacity', 5000, 'sample memory capacity')
 cmd:option('-episodes', 10, 'number of episodes to simulate')
+cmd:option('-quiet', false, 'produce less output')
 cmd:option('-momentum', 0.5, 'momentum')
 cmd:option('-max-time', 75, 'maximum length of games in clock ticks.')
 cmd:option('-rate', 0.1, 'learning rate')
-cmd:option('-save', 'model.t7', 'Filename to save model to.')
+cmd:option('-save', '', 'Filename to save model to (or read from when -mode test).')
 cmd:option('-initial-epsilon', 0.1, 'initial random choice probability, epsilon')
 cmd:option('-final-epsilon', 0.02, 'final random choice probability, epsilon')
 cmd:option('-decay-epsilon-over', 500, 'Number of episodes over which to decay epsilon')
-cmd:option('-gamma', 0.95, 'discounting parameter, gamma')
+cmd:option('-gamma', 0.99, 'discounting parameter, gamma')
 cmd:option('-regularization', 1e-05, 'weight-decay regularization')
 cmd:option('-prefix', 'model', 'saved model prefix')
+cmd:option('-simple', false, 'whether to use the simple feed-forward model')
 params = cmd:parse(arg)
 
 sceneLetters = {[-3]="w", [-2]="b", [-1]="d", "D", "B", "W"}
@@ -152,7 +154,7 @@ function reward(env, oldEnv)
     r = -2
   elseif env.ticks > maxTime then
     r = -1
-  elseif p == "B" and oldEnv ~= "B" then
+  elseif p == "B" and oldP ~= "B" then
     r = 1
   end
   return r
@@ -165,8 +167,15 @@ end
 -- If the reward is negative, the game is over.
 function evolve(env, action)
   local z = {size=env.size, speed=env.speed, goal=env.goal}
+  -- Calculate the players new position.
+  z.player = env.player + (action-2)
+  if z.player <= 0 then
+    z.player = 1
+  elseif z.player > z.size then
+    z.player = z.size
+  end
   -- See if the player is on the boat.
-  local onBoat = sceneLetters[env.scene[env.player]] == "B"
+  local onBoat = sceneLetters[env.scene[z.player]] == "B"
   -- Calculate the new position of the boat.
   z.boat = env.boat + env.speed/waterTiles(env)*env.direction
   -- Reflect the boat away from the edges if necessary, in which case
@@ -180,18 +189,9 @@ function evolve(env, action)
   else
     z.direction = env.direction
   end
-  -- If the player was on the boat, update his position to the boat's new position.
+  -- If the player is on the boat, update his position to the boat's new position.
   if onBoat then
     z.player = boatTile(z)
-  else
-    z.player = env.player
-  end
-  -- Calculate the players new position.
-  z.player = z.player + (action-2)
-  if z.player <= 0 then
-    z.player = 1
-  elseif z.player > z.size then
-    z.player = z.size
   end
   -- Render the new scene.
   z.scene = renderScene(z)
@@ -267,21 +267,24 @@ function play(simulator)
   copas.loop()
 end
 
--- Update x, shifting one vector off the left side, and adding the new vector
--- on the right.
+-- Return a tensor with the first row corresponding to the current
+-- scene, and the subsequent rows corresponding to x(t+1) - x(t) difference
+-- frames.
 function phi(history)
   local x = torch.Tensor(histLen, history[1].size)
   if #history < histLen then
     error("Insufficient history")
   end
   -- Copy the last `histLen` scenes into x
-  for i=1,histLen do
-    x[i]:copy(history[#history-histLen+i].scene)
+  x[1]:copy(history[#history].scene)
+  for i=2,histLen do
+    local prev = #history - (i - 1)
+    x[i]:copy(history[prev].scene - history[prev+1].scene)
   end
   return x
 end
 
--- The seqLenghs will be a batchSize-length vector of the constant, histSize. This
+-- The seqLengths will be a batchSize-length vector of the constant, histSize. This
 -- is an artifact of how I've implemented GRUChain.
 seqLengths = torch.Tensor(params.batch):fill(histLen)
 
@@ -340,6 +343,26 @@ function regularizationMask(net)
   return mask
 end
 
+function makeSimpleNet(par)
+  local ns = {}
+  -- The net should receive a tuple {x,seqLengths}
+  ns.inTuple = nn.Identity()()
+  ns.x = nn.SelectTable(1)(ns.inTuple)
+  ns.xr = nn.Reshape(gameSize*histLen)(ns.x)
+  ns.h = nn.Tanh()(nn.Linear(gameSize*histLen, par.hidden)(ns.xr))
+  ns.Q = nn.Linear(par.hidden, numActions)(ns.h)
+  ns.net = nn.gModule({ns.inTuple}, {ns.Q})
+  ns.net.par, ns.net.gradPar = ns.net:getParameters()
+  ns.net.par:uniform(-0.05, 0.05)
+  ns.net.gradPar:zero()
+  -- Additional step to condition on action
+  ns.givenActionInput = nn.Identity()()
+  ns.givenActionMasked = nn.CMulTable()(ns.givenActionInput)
+  ns.givenActionSum = nn.Sum(1, 1)(ns.givenActionMasked)
+  ns.givenAction = nn.gModule({ns.givenActionInput},{ns.givenActionSum})
+  return ns
+end
+
 function makeNet(par)
   local ns = {}
   -- The net should receive a tuple {x,seqLengths}
@@ -370,6 +393,38 @@ function Q(x, net)
   return net:forward({x,lengths})
 end
 
+function test(model, par)
+  local M = par['episodes']
+  local T = par['max-time']+5
+  local gamesWon = 0
+  for i=1,M do
+    local s = {firstEnvironment()}
+    for t=2,histLen do
+      s[t] = evolve(s[t-1], stay)
+    end
+    for t=histLen,T do
+      local xt = phi(s)
+      local q = Q(xt, model.net)
+      local _, bestQ = q:max(2)
+      local action = bestQ[1][1]
+      s[t+1], r = evolve(s[t], action)
+      if not par['quiet'] then
+        print("> " .. s[t].picture .. " took " .. action .. " to " .. s[t+1].picture .. ', reward: ' .. r)
+        print(nn.SoftMax():forward(q))
+      end
+      -- If the game is over, start the next episode
+      if s[t+1].gameOver then
+        if r == winReward then
+          gamesWon = gamesWon + 1
+        end
+        print("final reward: " .. r .. " iter " .. t)
+        break
+      end
+    end
+  end
+  print("Won " .. gamesWon .. " out of " .. M)
+end
+
 function train(par)
   local learningRate = par['rate']
   local momentum = par['momentum']
@@ -380,7 +435,12 @@ function train(par)
   local batchSize = par['batch']
   -- Current index into D
   local d = 1
-  local model = makeNet(par)
+  local model
+  if par['simple'] then
+    model = makeSimpleNet(par)
+  else
+    model = makeNet(par)
+  end
   local theta = model.net.par
   local gradTheta = model.net.gradPar
   -- Only regularize non-bias parameters
@@ -396,7 +456,7 @@ function train(par)
     if i < decayOver then
       epsilon = initialEpsilon + (i-1)*(finalEpsilon - initialEpsilon)/decayOver
     end
-    print("episode: " .. i .. ", epsilon: " .. epsilon)
+    --print("episode: " .. i .. ", epsilon: " .. epsilon)
     -- Run the game forward enough to get histLen images
     local s = {firstEnvironment()}
     for t=2,histLen do
@@ -410,19 +470,21 @@ function train(par)
       local u = torch.uniform()
       local action
       local wasRandom = ''
+      local q = nil
       if u < epsilon then
         -- Random action
         action = math.floor(torch.uniform(1,4))
         wasRandom = 'R'
       else
-        local q = Q(xt, model.net)
-        dumpVec("softmax(q)", softmax:forward(q))
+        q = Q(xt, model.net)
         local _, bestQ = q:max(2)
         action = bestQ[1][1]
       end
-      print("> " .. s[t].picture .. " : " .. action .. wasRandom)
       s[t+1], r = evolve(s[t], action)
-      print("> " .. s[t+1].picture .. ', reward: ' .. r)
+      print("> " .. s[t].picture .. " took " .. action .. wasRandom .. " to " .. s[t+1].picture .. ', reward: ' .. r .. " episode " .. i .. " timestep " .. t)
+      if q ~= nil then
+        print(softmax:forward(q))
+      end
       xtp1 = phi(s)
       D[d] = {xt, action, r, xtp1}
       d = (d % capacity) + 1
@@ -431,28 +493,21 @@ function train(par)
       -- before the predictions so we don't mess up our nerual net state during
       -- the forward and backward passes.
       local targets = Q(example[2], model.net):max(2)
-      dumpVec("reward", example[4])
       local nonTerminal =
         torch.gt(torch.eq(example[4],0):double() + torch.eq(example[4],1):double(),0):double()
-      dumpVec("nonTerminal", nonTerminal)
-      print("num non-terminal: " .. nonTerminal:sum())
       local discountedFuture = nonTerminal:cmul(targets):mul(gamma)
-      dumpVec("discountedFuture", discountedFuture)
       local y = example[4] + discountedFuture
-      dumpVec("y", y)
       gradTheta:zero()
       -- Forward pass
       local predictions = model.net:forward({example[1],seqLengths})
       local givenAction = model.givenAction:forward({predictions, example[3]})
-      dumpVec("givenAction", givenAction)
       if torch.eq(example[4], 1):double():sum() > 0 then
-        print("batch includes getting on boat")
-        print(predictions:narrow(1,1,8))
+        --print("batch includes getting on boat")
+        --print(predictions:narrow(1,1,8))
       end
       local err = criterion:forward(givenAction, y)
       -- Backward pass
       local g = criterion:backward(givenAction, y)
-      dumpVec("gradGivenAction", g)
       g = model.givenAction:backward({predictions, example[3]}, g)
       model.net:backward({example[1],seqLengths}, g[1])
 
@@ -489,8 +544,20 @@ if params.mode == 'play' then
   play()
 elseif params.mode == "train" then
   model = train(params)
+  if params.save == '' then
+    params.save = params.prefix .. ".t7"
+  end
   torch.save(params.save, model)
+elseif params.mode == "test" then
+  if params.save == '' then
+    error("Must speficy -save <t7 file>")
+  end
+  model = torch.load(params.save)
+  test(model, params)
 elseif params.mode == "simulate" then
+  if params.save == '' then
+    error("Must speficy -save <t7 file>")
+  end
   model = torch.load(params.save)
   play(model)
 else
